65229ee87330c2b40cb90563a08a1e73976fa6ad
John Kyrus <jnk5y@yahoo.com>
2017-07-17 18:54:34 -0400

    Fixed install script

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ fi
 cp bin/pi_garage_alert.py /usr/local/sbin/
 cp etc/pi_garage_alert_config.py /usr/local/etc/
 cp init.d/pi_garage_alert /etc/init.d/
-chown $1 /usr/local/etc/pi_garage_alert_config.py:q
+chown $1 /usr/local/etc/pi_garage_alert_config.py
