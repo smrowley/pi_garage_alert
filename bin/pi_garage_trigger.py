4e36a1b189fc16c7b640b40e7d9dd53869a45f8d
John Kyrus <jnk5y@yahoo.com>
2017-07-24 10:39:41 -0400

    added argument detector on trigger script

--- bin/pi_garage_trigger.py
+++ bin/pi_garage_trigger.py
@@ -2,8 +2,12 @@
 import sys
 from multiprocessing.connection import Client
 
-address = ('localhost', 6000)
-conn = Client(address, authkey='secret password')
-conn.send_bytes(sys.argv[1])
-print conn.recv_bytes()
-conn.close()
+if len(sys.argv) < 2:
+    print "You must pass a parameter. (open, close, trigger, away, home, state)"
+
+else:
+    address = ('localhost', 6000)
+    conn = Client(address, authkey='secret password')
+    conn.send_bytes(sys.argv[1])
+    print conn.recv_bytes()
+    conn.close()
